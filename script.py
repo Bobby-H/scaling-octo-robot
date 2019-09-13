80469ff6789ecd2d2bf48f49ee8891037600f242
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2019-09-12 21:37:59 -0400

    added add_attractions func, appends attractions to respective destinations

--- script.py
+++ script.py
@@ -7,6 +7,7 @@ test_traveler = ["Erin Wilkes", "Shanghai, China", ['historical site', 'art']]
 
 attractions = [[] for destination in destinations]
 
+
 def get_destination_index(destination):
   destination_index = destinations.index(destination)
   return destination_index
@@ -15,6 +16,13 @@ def get_traveler_location(traveler):
   traveler_destination = traveler[1]
   traveler_destination_index = get_destination_index(traveler_destination)
   return traveler_destination_index
+
+def add_attraction(destination, attraction):
+  try:
+    destination_index = get_destination_index(destination)
+    attractions_for_destination = destination_index[destination_index].append(attraction)
+  except SyntaxError:
+    return
 
 test_destination_index = get_traveler_location(test_traveler)
 print(attractions)
