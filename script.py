394ad04866905ff73572abafb2a8ab961306018b
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2019-09-12 20:41:18 -0400

    added get_traveler_location function

--- script.py
+++ script.py
@@ -3,8 +3,13 @@ destinations = ["Paris, France",
                 "Los Angeles, USA",
                 "Sao Paulo, Brazil",
                 "Cairo, Egypt"]
-test_traveler = ["Erin Wilkes", "Shanghai, China", ['jistorical site', 'art']]
+test_traveler = ["Erin Wilkes", "Shanghai, China", ['historical site', 'art']]
 
 def get_destination_index(destination):
   destination_index = destinations.index(destination)
   return destination_index
+
+def get_traveler_location(traveler):
+  traveler_destination = traveler[1]
+
+traveler_destination_index = get_destination_index(traveler_destination)
